3f081a31ec1d9ab321019ccd919ddb884f1511c9
ubu-prd-app1 <minhtuan2712@gmail.com>
2014-09-05 00:25:28 +1000

    remove abn web service

--- src/main/java/au/net/transtech/tuan/pojo/abn/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://abr.business.gov.au/ABRXMLSearch/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package au.net.transtech.tuan.pojo.abn;
